89b9f5c02f7854075f0dffedde71b453af0a7f26
Duncan Ryan <duncancryan1@gmail.com>
2020-07-22 22:56:51 +0100

    function refactored, works as intended

--- src/compare.py
+++ src/compare.py
@@ -3,3 +3,5 @@ def compare(num_1, num_2):
         return f"{num_1} is greater than {num_2}"
     elif num_1 < num_2:
         return f"{num_1} is less than {num_2}"
+    elif num_1 == num_2:
+        return "both numbers are equal"
