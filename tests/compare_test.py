23dc6885a1ae19ab8751a7addb1fa1386f08266f
Duncan Ryan <duncancryan1@gmail.com>
2020-07-22 22:48:19 +0100

    test for less than functionality added and failed

--- tests/compare_test.py
+++ tests/compare_test.py
@@ -6,3 +6,6 @@ class TestCompare(unittest.TestCase):
 
     def test_compare_3_1_returns_3_is_greater_than_1(self):
         self.assertEqual("3 is greater than 1", compare(3, 1))
+
+    def test_compare_3_6_returns_3_is_less_than_6(self):
+        self.assertEqual("3 is less than 6", compare(3, 6))
